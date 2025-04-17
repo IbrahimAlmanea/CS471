aa5d413006b4a9202f33bcd6ef9b84684062f037
IbrahimAlmanea <ibrahimalmanea.b@gmail.com>
2025-04-17 10:24:41 +0300

    lab3 complete

--- DjangoProjects/libraryproject/apps/bookmodule/views.py
+++ DjangoProjects/libraryproject/apps/bookmodule/views.py
@@ -5,3 +5,13 @@ def index(request):
 
 def index2(request, val1 = 0):
     return HttpResponse("value1 = "+str(val1))
+
+def viewbook(request, bookId):
+ # assume that we have the following books somewhere (e.g. database)
+ book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+ book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+ targetBook = None
+ if book1['id'] == bookId: targetBook = book1
+ if book2['id'] == bookId: targetBook = book2
+ context = {'book':targetBook} # book is the variable name accessible by the template
+ return render(request, 'bookmodule/show.html', context)
