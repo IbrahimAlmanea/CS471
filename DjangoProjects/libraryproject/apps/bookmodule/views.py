1ff91c9531bbcc1389522f1f994198be3b5f0173
IbrahimAlmanea <ibrahimalmanea.b@gmail.com>
2025-04-17 14:19:47 +0300

    lab4 complete

--- DjangoProjects/libraryproject/apps/bookmodule/views.py
+++ DjangoProjects/libraryproject/apps/bookmodule/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render 
 def index(request):
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
+
+
+
+
+
+'''
+def index(request):
     name = request.GET.get("name") or "world!"
     return render(request, "bookmodule/index.html" , {"name": name})
 
@@ -14,4 +28,5 @@ def viewbook(request, bookId):
  if book1['id'] == bookId: targetBook = book1
  if book2['id'] == bookId: targetBook = book2
  context = {'book':targetBook} # book is the variable name accessible by the template
- return render(request, 'bookmodule/show.html', context)
+ return render(request, 'bookmodule/show.html', context) 
+'''
